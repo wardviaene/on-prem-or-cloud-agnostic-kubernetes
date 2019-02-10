38ed2943b0deb899d67458a58819cf3ad9f18b70
cmndrsp0ck <barajasfa@gmail.com>
2019-02-10 01:52:10 -0800

    gitignore and Vagrantfile

--- scripts/install-kubernetes.sh
+++ scripts/install-kubernetes.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "installing docker"
-apt-get update
+apt-get remove docker docker-engine docker.io containerd runc;
+apt-get update;
 apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -12,16 +13,18 @@ add-apt-repository \
    "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
 
-echo "installing kubernetes"
-apt-get update && apt-get install -y apt-transport-https
+echo "installing kubernetes";
+apt-get update;
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get update;
+apt-get install -y kubelet kubeadm kubectl;
+sleep 1;
+apt-mark hold kubelet kubeadm kubectl;
 
 # DigitalOcean without firewall (IP-in-IP allowed) - or any other cloud / on-prem that supports IP-in-IP traffic
 # echo "deploying kubernetes (with calico)..."
@@ -32,8 +35,8 @@ apt-get install -y kubelet kubeadm kubectl
 
 
 # DigitalOcean with firewall (VxLAN with Flannel) - could be resolved in the future by allowing IP-in-IP in the firewall settings
-echo "deploying kubernetes (with canal)..."
-kubeadm init --pod-network-cidr=10.244.0.0/16 # add --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
-export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/rbac.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/canal.yaml
+echo "deploying kubernetes (with canal)...";
+kubeadm init --pod-network-cidr=10.244.0.0/16; # add --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
+export KUBECONFIG=/etc/kubernetes/admin.conf;
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/rbac.yaml;
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/canal.yaml;
